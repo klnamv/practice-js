5866c6eeaeb0f35714016a331c331f4e5f49f35f
klnamv <svitlana.dvoriankina@nure.ua>
2024-01-24 19:47:30 +0200

    created and updated color palette

--- day1/project/script.js
+++ day1/project/script.js
@@ -1,19 +1,28 @@
-function changeColors(){
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.background = '#' + randomColor;
-    color.innerHTML = '#' + randomColor;
+function getRandomColor(){
+    return Math.floor(Math.random()*16777215).toString(16);
 }
 
-one.addEventListener('click', changeColors);
-changeColors();
+function changeColors(containerId, colorId){
+    let randomColor = getRandomColor();
+    const elem = document.getElementById(containerId);
+    elem.style.background = '#' + randomColor;
+    const col = document.getElementById(colorId);
+    col.innerHTML = randomColor;
+}
 
+function changeAllColors(){
+    changeColors('randcolor1', 'color1');
+    changeColors('randcolor2', 'color2');
+    changeColors('randcolor3', 'color3');
+    changeColors('randcolor4', 'color4'); 
+    changeColors('randcolor5', 'color5');
+}
 
-// let index = 0;
-// let colors = ['red', 'darkblue', 'orange', 'yellow', 'green', 'purple'];
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Space' && event.target === document.body) {
+        event.preventDefault(); 
+        changeAllColors();
+    }
+});
 
-// function changeColors(){
-//     if (index > colors.length - 1)
-//         index = 0;
-
-//     document.getElementsByTagName('body')[0].style.background = colors[index++];   
-// }
+window.onload = changeAllColors;
