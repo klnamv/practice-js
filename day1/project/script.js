db9444b2fb980cc337c6594f1bae3dd7657fe3e5
klnamv <svitlana.dvoriankina@nure.ua>
2024-01-24 15:19:13 +0200

    added functionto generate random color and the design has been redesigned

--- day1/project/script.js
+++ day1/project/script.js
@@ -1,9 +1,19 @@
-let index = 0;
-let colors = ['red', 'darkblue', 'orange', 'yellow', 'green', 'purple'];
-
 function changeColors(){
-    if (index > colors.length - 1)
-        index = 0;
-
-    document.getElementsByTagName('body')[0].style.background = colors[index++];   
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.background = '#' + randomColor;
+    color.innerHTML = '#' + randomColor;
 }
+
+one.addEventListener('click', changeColors);
+changeColors();
+
+
+// let index = 0;
+// let colors = ['red', 'darkblue', 'orange', 'yellow', 'green', 'purple'];
+
+// function changeColors(){
+//     if (index > colors.length - 1)
+//         index = 0;
+
+//     document.getElementsByTagName('body')[0].style.background = colors[index++];   
+// }
