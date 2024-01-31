311491d23db0bbc4ff90bebbd5bfbb24546cf948
klnamv <117654777+klnamv@users.noreply.github.com>
2024-01-31 21:39:21 +0200

    solved  3 problems

---
+++ day3/problems.js
@@ -0,0 +1,27 @@
+// solved problems from website: JSchallenger
+// javascript fundamentals
+
+// check if a numbera whole numba
+function myFunc(a){
+    return (a * 10) % 10 === 0 ? true : false;
+};
+
+function myFunc1(a){
+    return a - Math.floor(a) === 0;
+};
+
+// multipltion, dividing, and comparison operators;
+function myFunction(a, b){
+    return a < b ? a / b : a * b;
+};
+
+// round a number to decimal places
+function myFunction(a) {
+    return Math.round(a * 100) / 100;
+};
+
+function myFunction1(a) {
+    return Number(a.toFixed(2));
+}
+
+//split numbers into digits
